357379ac05bb1a890f6519a7a950df9eae046bc2
simonmair <simon.mair1996@gmx.at>
2018-03-25 21:53:48 +0200

    Revert "gui initital commit"
    This reverts commit fe0ce1a6eac6158d0da0407a38ac96e36a652f74.

--- Application/FrontGuestGui/src/at/fhv/itb4/teamA/Main.java
+++ Application/FrontGuestGui/src/at/fhv/itb4/teamA/Main.java
@@ -1,40 +1,23 @@
 package at.fhv.itb4.teamA;
 	
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-
-	private Stage _mainWindow;
-	private BorderPane _mainView;
-	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			_mainWindow = primaryStage;
-			_mainWindow.setTitle("FrontGuest v0.1");
-			
-			loadMainView();
-			
-			//show the scene containing the main view
-			Scene scene = new Scene(_mainView);
-			_mainWindow.setScene(scene);
-			_mainWindow.show();
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	private void loadMainView() throws IOException {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
-		_mainView = (BorderPane) loader.load();
 	}
 	
 	public static void main(String[] args) {
